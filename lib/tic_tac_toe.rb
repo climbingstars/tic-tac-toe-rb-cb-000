b79b6a9e4db18737869518776d9fb0cddb65b902
Kyle Sanders <kyle.sanders.315@gmail.com>
2018-10-13 08:14:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -96,6 +96,8 @@ def winner(board)
     return "X"
   elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
     return "O"
+  elsif draw?(board)
+    nil
   end
   nil
 end
