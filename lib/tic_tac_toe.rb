e3769cb3bb29c0fc99e570790f9261cc1ee3101f
Kyle Sanders <kyle.sanders.315@gmail.com>
2018-10-13 07:12:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,7 +68,7 @@ end
 
 def won?(board)
 WIN_COMBINATIONS.each do |win_combo|
-  if win_combo
+  if player_move?(board, win_combo, "X") || player_move?(board, win_combo, "O")
   true
 elsif board[win_combo] == "X" || board[win_combo] == "O"
   false
