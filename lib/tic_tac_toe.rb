3a1307dac8803a3f2a7bbf726a2bdc8123b6a190
Kyle Sanders <kyle.sanders.315@gmail.com>
2018-10-13 08:09:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,7 +91,7 @@ def over?(board)
 end
 
 def winner(board)
-  WIN_COMBINATIONS.detect do |win_combo|
+  WIN_COMBINATIONS.each do |win_combo|
   if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X" (board[win_combo[3]]) == "X" && (board[win_combo[4]]) == "X"
     && (board[win_board[5]]) == "X" && (board[win_board[6]]) == "X" && (board[win_board[7]]) == "X" && (board[win_board[8]]) == "X"
   elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
